cf8bfa09343ced25f0831f0c4b448e43f022d438
CHHIEVTONG LY <chhievtongly@gmail.com>
2025-01-11 23:23:41 +0700

    VIP GENERATE USING NODE JS

---
+++ createLink.js
@@ -0,0 +1,28 @@
+function createLink(bot, msg, chatID, groupId, groupName) {
+  const chatId = chatID;
+  const expiredDate = Math.floor(Date.now() / 1000) + 5 * 60; // 5 minutes
+  var vipID = groupId;
+  var vipName = groupName;
+
+  if (msg.chat.username == "VVIPGirls_Support" || chatId == "7435194573") {
+    bot
+      .createChatInviteLink(vipID, {
+        name: `${groupName} created by ${
+          msg.chat.username || msg.chat.first_name
+        }`,
+        expire_date: expiredDate,
+        member_limit: 1,
+      })
+      .then((groupLink) => {
+        bot.sendMessage(
+          chatId,
+          `🔗 Here's your single-use invite link for ***${vipName}*** \n\n ${groupLink.invite_link}`,
+          { parse_mode: "Markdown", disable_web_page_preview: true }
+        );
+      });
+  } else {
+    bot.sendMessage(chatId, "You are not authorized to use this command.");
+  }
+}
+
+module.exports = { createLink };
---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+const { createLink } = require("./createLink");
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.BOT_TOKEN;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("App is running!");
+});
+
+const vipGroupID = [
+  {
+    name: "VIP Group",
+    id: process.env.VIP_GROUP,
+  },
+  {
+    name: "OF KH",
+    id: process.env.OF_GROUP,
+  },
+  {
+    name: "Thary",
+    id: process.env.THARY_GROUP,
+  },
+];
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const message = `Hello ${chatId}`;
+  bot.sendMessage(chatId, message, {
+    reply_markup: {
+      keyboard: [["👑 VIP", "💎 OF KH", "😩 Thary OF"]],
+      resize_keyboard: true,
+    },
+  });
+});
+
+bot.onText("👑 VIP", (msg) => {
+  const chatId = msg.chat.id;
+  const groupID = vipGroupID[0].id;
+  const groupName = vipGroupID[0].name;
+  createLink(bot, msg, chatId, groupID, groupName);
+});
+
+bot.onText("💎 OF KH", (msg) => {
+  const chatId = msg.chat.id;
+  const groupID = vipGroupID[1].id;
+  const groupName = vipGroupID[1].name;
+  createLink(bot, msg, chatId, groupID, groupName);
+});
+bot.onText("😩 Thary OF", (msg) => {
+  console.log(msg);
+
+  const chatId = msg.chat.id;
+  const groupID = vipGroupID[2].id;
+  const groupName = vipGroupID[2].name;
+  createLink(bot, msg, chatId, groupID, groupName);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express server is running on port ${PORT}`);
+});
